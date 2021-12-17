de8ab9b46bc02dbcedcf1ea475a6ed872dc4c345
Huan80805 <52092671+Huan80805@users.noreply.github.com>
2021-12-17 22:26:40 +0800

    add rf

--- train.py
+++ train.py
@@ -8,17 +8,31 @@ from sklearn import svm
 from sklearn.naive_bayes import MultinomialNB
 from sklearn_genetic import GAFeatureSelectionCV 
 from sklearn.neighbors import KNeighborsClassifier as KNN
+from sklearn.ensemble import RandomForestClassifier as RF
 parser = argparse.ArgumentParser()
 parser.add_argument('--feature_selection', default=False, action='store_true', help='enable feature selection')
 parser.add_argument('--GA', default=False, action='store_true', help='Genetic Algorithm')
 parser.add_argument('--NB', default=False, action='store_true', help='Naive Bayes')
 parser.add_argument('--KNN', default=False, action='store_true', help='k nearest neighbor')
 parser.add_argument('--SVM', default=False, action='store_true', help='support vector machine')
+parser.add_argument('--kernel', default='linear', type=str, choices=['linear', 'sigmoid', 'rbf'])
+parser.add_argument('--RF', default=False, action='store_true', help='random forest') 
 args = parser.parse_args()
 df = pd.read_csv("archive/emails.csv")
 x = df.iloc[:, 1:3001]
 y = df.iloc[:, -1].values
 train_x,test_x,train_y,test_y = train_test_split(x,y,test_size = 0.25)
+
+if args.NB:
+    clf = MultinomialNB(alpha=1.9)
+elif args.KNN:
+    clf = KNN(n_neighbors=5) #tune:weights, p
+elif args.SVM:
+    clf = svm.SVC(kernel=args.kernel)
+elif args.RF:
+    clf = RF(n_estimators=20, max_depth=4)
+else:
+    sys.exit('must specify classifier')
 #feature selection
 if args.feature_selection:
     seed = 123
@@ -26,7 +40,6 @@ if args.feature_selection:
     random.seed(seed)
     if args.GA :
         # use multinomialNB to evaluate genetic performance
-        clf = MultinomialNB(alpha=1.9)
         GA = GAFeatureSelectionCV(
             estimator=clf,
             scoring="accuracy",
@@ -40,14 +53,6 @@ if args.feature_selection:
         predict = GA.predict(test_x.values[:, features])
         print('Acc:', 100.*(np.mean(predict==test_y)))
 else: 
-    if args.NB:
-        clf = MultinomialNB(alpha=1.9)
-    elif args.KNN:
-        clf = KNN(n_neighbors=5) #tune:weights, p
-    elif args.SVM:
-        clf = svm.SVC(kernel='linear') #tune:rbf kernel, sigmoid
-    else:
-        sys.exit('must specify classifier')
     clf.fit(train_x, train_y)
     predict = clf.predict(test_x)
     print('Acc:', 100.*(np.mean(predict==test_y)))
